03038955aea95144743e68e3a83a6657a482c5f5
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-11 21:50:17 +0800

    create install launch files

---
+++ launch/number_app.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    number_publisher_node = Node(
+        package="my_py_pkg",
+        executable="number_publisher"
+    )
+
+    number_counter_node = Node(
+        package="my_cpp_pkg",
+        executable="number_counter"
+    )
+
+    ld.add_action(number_publisher_node)
+    return ld
